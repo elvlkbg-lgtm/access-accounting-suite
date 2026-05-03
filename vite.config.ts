6807c8b7fea6e9f7f7777de79a0fa92cc7cc930b
elvlkbg-lgtm <elvlkbg@gmail.com>
2026-05-03 10:27:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/access-accounting-suite/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
